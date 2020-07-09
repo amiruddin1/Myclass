c62155ae8b7db8941f1e69fea101e3829df33e50
amiruddin1 <67670878+amiruddin1@users.noreply.github.com>
2020-07-09 08:41:44 +0530

    Create Myclass.java

---
+++ Myclass.java
@@ -0,0 +1,62 @@
+Public class Myclass{
+Char name[30];
+Char Address[50];
+Int salary;
+Int year_joining;
+
+Public static void main{
+Myclass obj1 = new Myclass();
+Myclass obj2 = new Myclass();
+Myclass obj3 = new Myclass();
+
+System.out.println("Enter Employee's name: ");
+Scanner obj1.name= new scanner (System.in);
+System.out.println("Enter Employee's Year of Joining: ");
+Scanner obj1.year_joining= new scanner (System.in);
+System.out.println("Enter Employee's Salary: ");
+Scanner obj1.salary= new scanner (System.in);
+System.out.println("Enter Employee's Address: ");
+Scanner obj1.address= new scanner (System.in);
+
+System.out.println("Enter Employee's name: ");
+Scanner obj2.name= new scanner (System.in);
+System.out.println("Enter Employee's Year of Joining: ");
+Scanner obj2.year_joining= new scanner (System.in);
+System.out.println("Enter Employee's Salary: ");
+Scanner obj2.salary= new scanner (System.in);
+System.out.println("Enter Employee's Address: ");
+Scanner obj2.address= new scanner (System.in);
+
+System.out.println("Enter Employee's name: ");
+Scanner obj3.name= new scanner (System.in);
+System.out.println("Enter Employee's Year of Joining: ");
+Scanner obj3.year_joining= new scanner (System.in);
+System.out.println("Enter Employee's Salary: ");
+Scanner obj3.salary= new scanner (System.in);
+System.out.println("Enter Employee's Address: ");
+Scanner obj3.address= new scanner (System.in);
+
+
+System.out.println("1 Employee's Name is: "+obj1.name);
+System.out.println("2 Employee's Name is: "+obj2.name);
+System.out.println("3 Employee's Name is: "+obj3.name);
+
+System.out.println("1 Employee's Salary is: "+obj1.salary);
+System.out.println("2 Employee's Salary is: "+obj2.salary);
+System.out.println("3 Employee's Salary is: "+obj3.salary);
+
+System.out.println("1 Employee's Year of Joining is: "+obj1.year_joining);
+System.out.println("2 Employee's Year of Joining is: "+obj2.year_joining);
+System.out.println("3 Employee's Year of Joining is: "+obj3.year_joining);
+
+System.out.println("1 Employee's Address is: "+obj1.Address);
+System.out.println("2 Employee's Address is: "+obj2.Address);
+System.out.println("3 Employee's Address is: "+obj3.Address);
+}
+}
+
+
+
+
+
+
